73289c419be2f9743b1be73f5dfd95177c1424fa
hazratgs <hazratgs@gmail.com>
2018-07-13 14:03:22 +0300

    Add reducer bookmarks

--- src/reducers/App.js
+++ src/reducers/App.js
@@ -1,9 +1,16 @@
-import { } from '../constants/App'
+import {
+  GET_ALL_BOOKMARKS
+} from '../constants/App'
 
-const initialState = { }
+const initialState = {
+  items: []
+}
 
 export default function appState (state = initialState, action) {
   switch (action.type) {
+    case GET_ALL_BOOKMARKS:
+      return { ...state, items: action.payload }
+
     default:
       return state
   }
