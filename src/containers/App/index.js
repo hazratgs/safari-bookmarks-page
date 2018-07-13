32ebd824248f8eebd6003a4954c4121572592861
hazratgs <hazratgs@gmail.com>
2018-07-13 10:25:03 +0300

    Initial commit

---
+++ src/containers/App/index.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+import { withRouter } from 'react-router-dom'
+
+@withRouter
+export default class App extends PureComponent {
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store/configureStore'
+
+import App from './containers/App'
+
+const render = (Component) => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Component/>
+        </div>
+      </Router>
+    </Provider>,
+    document.getElementById('root')
+  )
+}
+
+render(App)
+
+// Hot Module Replacement
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    render(App)
+  })
+}
