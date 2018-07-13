4e36256a0ec56819bbfbd6b95737abe81ec42edf
hazratgs <hazratgs@gmail.com>
2018-07-13 14:02:48 +0300

    Add action load chrome bookmarks

--- src/actions/App.js
+++ src/actions/App.js
@@ -1,2 +1,19 @@
-import { } from '../constants/App'
+import {
+  GET_ALL_BOOKMARKS
+} from '../constants/App'
 
+export function getAllBookmarks () {
+  return dispatch => {
+    window.chrome.bookmarks.getTree((array) => {
+      const elements = array[0].children[0].children.map(item => ({
+        title: item.title.substring(0, 32) + '...',
+        url: item.url
+      }))
+      console.log(elements, array)
+      dispatch({
+        type: GET_ALL_BOOKMARKS,
+        payload: elements
+      })
+    })
+  }
+}
