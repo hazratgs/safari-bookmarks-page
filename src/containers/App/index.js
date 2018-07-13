c4d8c980c5fb692c79505d826a0c867491bfce5d
hazratgs <hazratgs@gmail.com>
2018-07-13 14:04:34 +0300

    Add components extension

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,11 +1,33 @@
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { withRouter } from 'react-router-dom'
+import { getAllBookmarks } from '../../actions/App'
+
+import Title from '../../components/Title'
+import Background from '../../components/Background'
+import Elements from '../../components/Elements'
 
 @withRouter
+@connect(
+  state => ({
+    items: state.App.items
+  }),
+  dispatch => ({
+    getAllBookmarks: bindActionCreators(getAllBookmarks, dispatch)
+  })
+)
 export default class App extends PureComponent {
+  componentDidMount () {
+    this.props.getAllBookmarks()
+  }
+
   render () {
     return (
-      <div></div>
+      <Background>
+        <Title>Избранное</Title>
+        <Elements items={this.props.items}/>
+      </Background>
     )
   }
 }
